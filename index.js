d2f5dc050c967df7fae43db5461628335a73c7ac
Henry Troutman <henguin1001@gmail.com>
2020-12-11 19:00:52 -0800

    Added puppeteer to convert png to svg through a browser session

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 const nunjucks = require('nunjucks');
 const fs = require('fs');
-const sharp = require('sharp')
-var svg2img = require('svg2img');
-var btoa = require('btoa');
+const path = require('path');
+const puppeteer = require('puppeteer');
 
-nunjucks.configure({ autoescape: true });
-const HTML = false
+const HTML = true;
 const WIDTH = 1536;
 const HEIGHT = 862;
 const cell_width = 135;
 const even_offset = 60;
-
-
-model = [];
-var paths = fs.readdirSync('./img/').map(file => {
-  return `./img/${file}`;
-});
-
-for (var x = 0; x < WIDTH; x+=cell_width) {
-  for (var y = 0; y < HEIGHT; y+=cell_width) {
-    image_index = getRandomInt(0,paths.length-1);
-    console.log(image_index);
-    model.push({
-      x:x + (y%2)*even_offset,
-      y:y,
-      href:paths[image_index],
-      width:cell_width
-    });
-  }
-}
-
-var output = nunjucks.render('template.njs', { width:1536, height:862, grid:model, html:HTML });
-if(HTML){
-  fs.writeFileSync('./output/output.html', output);
-} else {
-
-  fs.writeFileSync('./output/output.svg', output);
-
-  //1. convert from svg string
-  svg2img(output, function(error, buffer) {
-      //returns a Buffer
-      fs.writeFileSync('./output/output.png', buffer);
-  });
-
-
-
-}
+const image_path = './img/';
+const output_path = './output/';
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const main = async function(){
+  model = [];
+  nunjucks.configure({ autoescape: true });
+  // find all the image paths in the folder
+  images = fs.readdirSync(image_path).map(filename=>`../img/${filename}`);
+  for (var x = 0; x < WIDTH; x+=cell_width) {
+    for (var y = 0; y < HEIGHT; y+=cell_width) {
+      image_index = getRandomInt(0,images.length-1);
+      model.push({
+        x:x + (y%2)*even_offset,
+        y:y,
+        href:images[image_index],
+        width:cell_width
+      });
+    }
+  }
+  // render the svg template
+  var output = nunjucks.render('template.njs', { width:WIDTH, height:HEIGHT, grid:model, html:HTML });
+  // if the we are exporting html output to an html file otherwise svg
+  var output_path;
+  if(HTML){
+    output_path = './output/output.html';
+  } else {
+    output_path = './output/output.svg';
+  }
+  // save the data to the file
+  fs.writeFileSync(output_path, output);
+
+  // create a browser instance to take a screenshot of the svg
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: WIDTH, height: HEIGHT })
+  await page.goto(`file:${path.join(__dirname, output_path)}`);
+  await page.screenshot({path: './output/output.png'});
+
+  await browser.close();
+}
+
+main()
+  .then(()=>console.log("Success"))
+  .catch(error=>console.error(error));
